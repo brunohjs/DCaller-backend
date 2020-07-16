0b8b30a17a3afb0fd424e0fb699d78969e218771
Bruno <brunohjs@gmail.com>
2020-07-15 21:02:25 -0300

    Finalização da organização do repositório

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
+require('dotenv').config()
 const express = require('express');
 const routes = require('./routes'); 
 const cors = require('cors');
 
 const app = express();
 
+const port = process.env.PORT || 3000;
+
 app.use(cors());
 app.use(express.json());
-app.use('/api', routes);
+app.use('/', routes);
 
-app.listen(3000, () => {
-    console.log('Server listening on port 3001')
+app.listen(port, () => {
+    console.log('Server listening on port', port)
 });
